c79071172143e1f2ce46eb1bb76a4a1251a0dc96
allanpereira99 <allan.pereira.dev@gmail.com>
2022-05-13 13:09:46 +0000

    platina: add device_framework_compatibility_matrix.xml

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,6 +105,10 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/device_framework_compatibility_matrix.xml \
+    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_platina
 TARGET_RECOVERY_DEVICE_MODULES := libinit_platina
